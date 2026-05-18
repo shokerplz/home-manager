ae37e527dc77ef60ae6aa965ad085b1b3e42cddf
Ivan Kovalev <ivan@ikovalev.nl>
2026-05-18 12:38:45 +0000

    Revert "Switch to dolphin from nautilus"
    This reverts commit ef3ffa179ac4319a448dfda47a635dee62e04d0d

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -24,6 +24,7 @@
       enable = true;
       sessionVariables = {
         TERMINAL = "alacritty";
+        QT_QPA_PLATFORMTHEME = "adwaita";
       };
       historyControl = [
         "ignoredups"
