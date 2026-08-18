ffed4ad265a6898a9d094dd02153eada21fbb5c9
Ivan Kovalev <ivan4kovalev@gmail.com>
2026-08-19 01:48:36 +0200

    nvf: drop removed vim.languages.rust.lsp.opts

--- packages/nvf-config.nix
+++ packages/nvf-config.nix
@@ -69,18 +69,7 @@
         enable = true;
         extensions.crates-nvim.enable = true;
         format.enable = true;
-        lsp = {
-          enable = true;
-          opts = ''
-            ['rust-analyzer'] = {
-              cargo = {allFeature = true},
-              checkOnSave = true,
-              procMacro = {
-                enable = true,
-              },
-            },
-          '';
-        };
+        lsp.enable = true;
         treesitter.enable = true;
       };
 
