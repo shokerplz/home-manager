5a00893af61ed374b7274d06d1e0c7083468f66f
Ivan Kovalev <ivan@ikovalev.nl>
2026-09-25 13:24:16 +0200

    libreoffice changed upstream

--- modules/hosts/main-pc/_packages.nix
+++ modules/hosts/main-pc/_packages.nix
@@ -29,7 +29,7 @@
 in {
   home.packages = with pkgs; [
     vlc
-    libreoffice-qt6-fresh
+    libreoffice-qt-stable
     godot
     devenv
     orca-slicer-appimage
--- modules/hosts/pocket4/_packages.nix
+++ modules/hosts/pocket4/_packages.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     vlc
-    libreoffice-qt6-fresh
+    libreoffice-qt-stable
     godot
     devenv
     orca-slicer
